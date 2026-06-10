018da4fbccb80c58ebcd56412c21e74b5ef4e06e
Talleres Puente Alto <tallerespa@inacapmail.cl>
2026-06-09 20:51:53 -0400

    se actualiza proyecto

---
+++ script.js
@@ -0,0 +1,18 @@
+ var edad = 18;
+
+// scope alcance
+
+if (true) {
+    let y = 15;
+    console.log("variable y:"+y)
+}
+
+//Arreglo
+
+let alummnos = ["alexis", "nicolas", "Samuel", "Camilo", "Romané"];
+
+// console.log(alummnos);
+
+for (let i = 0; i < alummnos.length; i++) {
+    console.log(alummnos[i]);
+}
---
+++ scriptLogin.js
@@ -0,0 +1,48 @@
+// Funcion onclick
+// function irPagina(){
+//     window.location.href = "index.html";
+// }
+
+let boton = document.getElementById("btnIngresar");
+
+// boton.addEventListener("click", function(){
+//     console.log("Hiciste click en el boton");
+// });
+
+//Validacion de los input 
+// boton.addEventListener("click", function(){
+//     let usuario = document.getElementById("usuario").value;
+//     let clave = document.getElementById("clave").value;
+
+//     console.log("Usuario ingresado: " + usuario);
+//     console.log("Password ingresada: "+ clave);
+// });
+
+
+boton.addEventListener("click", function () {
+
+    let usuarioCorrecto = "admin";
+    let claveCorrecta = "1234";
+
+    let usuario = document.getElementById("usuario").value;
+    let clave = document.getElementById("clave").value;
+    let mensaje = document.getElementById("mensaje");
+
+    if (usuario === "" || clave === "" ){
+        mensaje.textContent = "Debes completar todos los campos";
+        mensaje.style.color = "orange";
+    }else if(usuario === usuarioCorrecto && clave === claveCorrecta){
+        mensaje.textContent = "Acceso Permitido. Redirigiendo en 3 segundos";
+        mensaje.style.color = "green";
+
+        setTimeout (function () {
+            window.location.href = "index.html"
+        }, 3000);
+    }else {
+        mensaje.textContent = "Usuario o contraseña incorrectos";
+        mensaje.style.color = "red";
+    }
+
+
+
+});
